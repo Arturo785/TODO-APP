90d0d43c7f2a4d55cc5ad7608091b5d3257b13ce
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2021-04-07 18:30:12 -0500

    Search functionality and basic flow usage

--- app/src/main/java/com/example/todoapp/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/tasks/TaskViewModel.kt
@@ -4,12 +4,22 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.todoapp.data.db.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 
 
 class TaskViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ): ViewModel(){
 
-    val tasks = taskDao.getTasks().asLiveData()
+    // gets changed on the searchView in fragment
+    val searchQuery = MutableStateFlow("")
+
+    // whenever the value of the flow changes execute this code and assign the result to itself
+    private val taskFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val tasks = taskFlow.asLiveData()
 
 }
--- app/src/main/java/com/example/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/tasks/TasksFragment.kt
@@ -1,13 +1,18 @@
 package com.example.todoapp.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentTasksBinding
+import com.example.todoapp.utils.onQueryTextListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,8 +29,13 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
         setupRecyclerView(binding)
         subscribeObservers()
+
+        setHasOptionsMenu(true)
     }
 
+
+
+    ///------------- Made by me ---------------------------
     private fun setupRecyclerView(binding: FragmentTasksBinding){
         binding.apply {
             recyclerViewTasks.apply {
@@ -40,6 +50,43 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
         viewModel.tasks.observe(viewLifecycleOwner, {
             tasksAdapter.submitList(it)
         })
+    }
+
+    ///--------------- Made by the SO ----------------------
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextListener{
+            // update search query
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_hide_completed_task -> {
+                item.isChecked = !item.isChecked
+
+                true
+            }
+            R.id.action_delete_all_completed_task -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
